1fc0b9fbc632ba686e42ddd8849c9ed58cef0c43
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2025-02-06 15:40:15 +0200

    addcomit_15.39_06.02.2025
    Додано серверний компонент, клієнтський компонент, копію серверного компоненту, який поміщено в клієнтський компонент, як дочірній.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import StatusLabel, { Status } from './components/active-label';
 import { headers } from 'next/headers';
 import AddCompanyButton from './components/add-company-button';
+import ServerComponent from './components/server-component';
+import ClientComponent from './components/client-component';
+import ServerComponentCopy from './components/server-component-copy';
 
 export default function Home() {
   console.log(headers());
@@ -10,6 +13,10 @@ export default function Home() {
       <main >
       <h1 className="text-xl">Da bliat sluchilos {new Date().toTimeString()}</h1>
       <AddCompanyButton />
+      <ServerComponent />
+      <ClientComponent>
+        <ServerComponentCopy />
+      </ClientComponent>
       </main>
 
   );
